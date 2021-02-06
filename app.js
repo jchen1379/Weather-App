095c46c6f4c8f3678113c0523eb3b8765058ed77
Jay Chen <55901916+jchen1379@users.noreply.github.com>
2021-02-06 10:59:15 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,68 +1,132 @@
 window.addEventListener('load', () => {
-	let long;
-	let lat;
-	let temperatureDescription = document.querySelector('.temperature-description');
-	let temperatureDegree = document.querySelector('.temperature-degree');
+	let long = -118.243683;
+	let lat = 34.052235;
 	let locationTimeZone = document.querySelector('.location-timezone');
-	let temperatureSection = document.querySelector('.temperature');
-	let temperatureSpan = document.querySelector('.temperature span');
-	
+	let temperatureDegree = document.querySelector('.temperature-degree');
+	let temperatureDescription = document.querySelector('.temperature-description');
+	let weatherIcon = document.querySelector('.icon');
+	let zipcodeInput = document.querySelector('.input-text');
+	let searchBtn = document.querySelector('.search-btn');
+	let errorMessage = document.querySelector('.error-message');
+
+	let api;
+	let dataFromAPI;
+	let temperatureUnit = ' \u00B0F';
+
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(position => {
 			long = position.coords.longitude;
 			lat = position.coords.latitude;
-
-			const proxy = 'https://cors-anywhere.herokuapp.com/';
-			const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
-			
-			// get data from API
-			fetch(api)
-				.then(response => {
-					return response.json();
-				})
-				.then(data => {
-					console.log(data);
-					// save the data from API for future uses
-					const {
-						temperature,
-						summary,
-						icon
-					} = data.currently;
-					
-					// set DOM Elements from the API
-					temperatureDegree.textContent = temperature;
-					temperatureDescription.textContent = summary;
-					let timezone = data.timezone.replace('_', '\u0020');
-					locationTimeZone.textContent = timezone.replace('/', ', ');
-					// Formula for celsius
-					let celsius = (temperature - 32) * (5 / 9);
-					
-					// set Icon
-					setIcons(icon, document.querySelector('.icon'));
-					
-					// change temperature to Celsius/Farenheit
-					temperatureSection.addEventListener('click', () =>{
-						if(temperatureSpan.textContent === "\u00BAF"){
-							temperatureSpan.textContent = "\u00BAC";
-							temperatureDegree.textContent = Math.floor(celsius);
-						}else{
-							temperatureSpan.textContent = "\u00BAF";
-							temperatureDegree.textContent = temperature;
-						}
-					})
-				});
-		});
-
-	} else {
-		locationTimeZone.textContent = "Please allow location access to get temperature information.";
+			render(long, lat, null);
+		})
 	}
 
-	function setIcons(icon, iconID){
-		const skycons = new Skycons({color: "white"});
-		const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+	render(long, lat, null)
+
+	searchBtn.addEventListener('click', () => getZipcodeLocation());
+	document.querySelector('.temperature').addEventListener('click', () => updateTemperatureUnit(dataFromAPI));
+	zipcodeInput.addEventListener('focus', () => errorMessage.textContent = "");
+	zipcodeInput.addEventListener("keyup", function(event) {
+		if (event.keyCode === 13) {
+			searchBtn.click();
+		}
+	})
+
+	function render(long, lat, city) {
+		api =
+			`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly,daily&appid=ecf3bcaacd721741ea58571e6fdda2ea`;
+		fetch(api)
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				// rendering currently information
+				dataFromAPI = data;
+				renderCurrentWeather(data, city);
+			})
+	}
+
+	function renderCurrentWeather(data, city) {
+		const {
+			temp,
+			weather,
+		} = data.current;
+
+		temperatureDegree.textContent = tempUnitConverter(temp) + temperatureUnit;
+		temperatureDescription.textContent = weather[0].main;
+		if (city === null) {
+			locationTimeZone.textContent = data.timezone.split("/")[1].replace("_", " ");
+		} else {
+			locationTimeZone.textContent = city;
+		}
+		setIcons(weather[0].icon, weatherIcon);
+	}
+
+	function getZipcodeLocation() {
+		const zipcode = zipcodeInput.value;
+		api = `https://zipcode-geolocation-api.herokuapp.com/api/zipcode/${zipcode}`
+		fetch(api) 
+			.then(res =>{
+				return res.json();
+			})
+			.then(data => {
+				if(data.length === 0){
+					errorMessage.textContent = "This zipcode is not in our database!";
+				}else{
+					errorMessage.textContent = "";
+					const {
+						long,
+						lat,
+						city,
+						state
+					} = data[0];
+					render(long, lat, `${city}, ${state}`);
+				}
+			})
+	}
+
+	function updateTemperatureUnit(data) {
+		// change the temperature display unit
+		temperatureUnit === " \u00B0F" ? temperatureUnit = " \u00B0C" : temperatureUnit = " \u00B0F";
+		temperatureDegree.textContent = tempUnitConverter(data.current.temp) + temperatureUnit;
+	}
+
+	function tempUnitConverter(temp) {
+		if (temperatureUnit === ' \u00B0F') {
+			return Math.floor(temp)
+		}
+		return Math.floor((temp - 32) * 5 / 9);
+	}
+
+
+	let iconIDAdapter = {
+		"01d": "CLEAR_DAY",
+		"01n": "CLEAR_NIGHT",
+		"02d": "PARTLY_CLOUDY_DAY",
+		"02n": "PARTLY_CLOUDY_NIGHT",
+		"03d": "CLOUDY",
+		"03n": "CLOUDY",
+		"04d": "CLOUDY",
+		"04n": "CLOUDY",
+		"09d": "RAIN",
+		"09n": "RAIN",
+		"10d": "SLEET",
+		"10n": "SLEET",
+		"11d": "RAIN",
+		"11n": "RAIN",
+		"13d": "SNOW",
+		"13n": "SNOW",
+		"50d": "FOG",
+		"50n": "FOG"
+	};
+
+	function setIcons(icon, iconID) {
+		const skycons = new Skycons({
+			color: "white"
+		});
+		const currentIcon = iconIDAdapter[icon];
 		skycons.play();
 		return skycons.set(iconID, Skycons[currentIcon]);
 	}
 
 });
-
